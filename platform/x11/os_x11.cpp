98e055f0bee1958b4a1dcd714127d2df837ed7ac
karroffel <therzog@mail.de>
2017-12-04 13:41:34 +0100

    create GLES 2 compatible context

--- platform/x11/os_x11.cpp
+++ platform/x11/os_x11.cpp
@@ -282,12 +282,21 @@ Error OS_X11::initialize(const VideoMode &p_desired, int p_video_driver, int p_a
 //print_line("def videomode "+itos(current_videomode.width)+","+itos(current_videomode.height));
 #if defined(OPENGL_ENABLED)
 
-	context_gl = memnew(ContextGL_X11(x11_display, x11_window, current_videomode, true));
+	ContextGL_X11::ContextType opengl_api_type = ContextGL_X11::GLES_2_0_COMPATIBLE;
+
+	context_gl = memnew(ContextGL_X11(x11_display, x11_window, current_videomode, opengl_api_type));
 	context_gl->initialize();
 
-	RasterizerGLES3::register_config();
-
-	RasterizerGLES3::make_current();
+	switch (opengl_api_type) {
+		case ContextGL_X11::ContextType::GLES_2_0_COMPATIBLE: {
+			// RasterizerGLES2::register_config();
+			// RasterizerGLES2::make_current();
+		} break;
+		case ContextGL_X11::ContextType::GLES_3_0_COMPATIBLE: {
+			RasterizerGLES3::register_config();
+			RasterizerGLES3::make_current();
+		} break;
+	}
 
 	context_gl->set_use_vsync(current_videomode.use_vsync);
 
